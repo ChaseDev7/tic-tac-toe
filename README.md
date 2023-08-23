801d4d7257f48ff671357a55c0b1218dafee05cf
ChaseDev7 <chasewebdev101@gmail.com>
2023-08-22 20:20:30 -0700

    Update styles, box id's

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Made with:
 
 - VSCode
 - Google Chrome (for testing)
+
+IMPORTANT NOTE: I do not own the copyright to any of the images or names used in this project. It is the property of DC Comics and Warner Bros.
--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const gameBoard = (() => {
+  // Creates array for storing player selections during game.
   const gameboardArray = ["", "", "", "", "", "", "", "", ""];
+
+  // Creates the game board and all the blocks inside of it.
   const boardContainer = document.querySelector("#board-container");
   const board = document.createElement("div");
   board.setAttribute("id", "game-board");
   boardContainer.appendChild(board);
+
+  for (i = 0; i < gameboardArray.length; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.setAttribute("id", "box " + i);
+    board.appendChild(box);
+  }
   return { gameboardArray };
 })();
 
