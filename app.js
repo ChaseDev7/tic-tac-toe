e2598de2e4e04c604a35783cb2028f8dbbc4c0e6
ChaseDev7 <chasewebdev101@gmail.com>
2023-08-22 23:49:38 -0700

    Add images, button code

--- app.js
+++ app.js
@@ -7,14 +7,38 @@ const gameBoard = (() => {
   const board = document.createElement("div");
   board.setAttribute("id", "game-board");
   boardContainer.appendChild(board);
+  let boxNumber = 1;
 
   for (i = 0; i < gameboardArray.length; i++) {
     const box = document.createElement("div");
     box.classList.add("box");
-    box.setAttribute("id", "box " + i);
+    box.setAttribute("id", "box-" + boxNumber++);
     board.appendChild(box);
   }
   return { gameboardArray };
 })();
+
+const Player = (player, selector) => {
+  const playerName = player;
+  const boardSelector = selector;
+  return { playerName, boardSelector }
+}
+
+// ADD FUNCTION INSTEAD OF ARGUMENT ONCE PLAYER SELECTED.
+const batman = Player("BATMAN", "X");
+const joker = Player("JOKER", "O");
+
+console.log(batman.playerName);
+console.log(batman.boardSelector);
+
+Array.from(document.getElementsByClassName("box"))
+  .forEach(function(element){
+    element.addEventListener("click", function() {
+      const boxID = element;
+      console.log(`${boxID} selected.`);
+      console.log(element);
+    });
+  });
+
 
 console.log(gameBoard.gameboardArray);
