c6cef7dcc862a3dec19c41b65ce1e566d1986be1
ChaseDev7 <chasewebdev101@gmail.com>
2023-08-22 19:20:20 -0700

    Add gameboard in jss, css

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("it works!");
+const gameBoard = (() => {
+  const gameboardArray = ["", "", "", "", "", "", "", "", ""];
+  const boardContainer = document.querySelector("#board-container");
+  const board = document.createElement("div");
+  board.setAttribute("id", "game-board");
+  boardContainer.appendChild(board);
+  return { gameboardArray };
+})();
+
+console.log(gameBoard.gameboardArray);
