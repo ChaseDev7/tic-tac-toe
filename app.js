8c5c891824537d873fcdeef8d27a6972cad01fbb
ChaseDev7 <chasewebdev101@gmail.com>
2023-08-25 01:20:28 -0700

    Add eventlisteners to boxes

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+// Factory function for players in the game.
+const Player = (player, selector) => {
+  const playerName = player;
+  const boardSelector = selector;
+
+  return { playerName, boardSelector }
+}
+
+let currentPlayer = "";
+
+// Individual players created with markers.
+const player = Player("Player", "X");
+const opponent = Player("Opponent", "O");
+
+//
+// Creates the game board with 9 squares to play the game.
+//
+
 const gameBoard = (() => {
   // Creates array for storing player selections during game.
-  const gameBoardArray = ["X", "X", "O", "X", "O", "X", "X", "O", "O"];
+  const gameBoardArray = ["", "", "", "", "", "", "", "", ""];
 
   // Creates the game board.
   const boardContainer = document.querySelector("#board-container");
@@ -10,43 +28,59 @@ const gameBoard = (() => {
   let boxNumber = 1;
 
   // Creates the blocks inside the game board for players to select.
-  for (i = 0; i < gameBoardArray.length; i++) {
-    const box = document.createElement("div");
-    box.classList.add("box");
-    box.setAttribute("id", "box-" + boxNumber++);
-    box.innerHTML = gameBoardArray[i];
-    board.appendChild(box);
+  const showGameBoardArray = () => {
+    
+    for (i = 0; i < gameBoardArray.length; i++) {
+      const box = document.createElement("div");
+      box.classList.add("box");
+      box.setAttribute("id", "box-" + boxNumber++);
+      box.setAttribute("data-box-id", i);
+      board.appendChild(box);
+    };
+  };
+
+  showGameBoardArray();
+  
+  const xMarker = document.querySelector("#x-marker");
+  xMarker.textContent = player.boardSelector;
+  const oMarker = document.querySelector("#o-marker");
+  oMarker.textContent = opponent.boardSelector;
+
+  function xSelectIsCurrentPlayer () {
+    currentPlayer = xMarker.textContent;
+    oMarker.removeEventListener("click", oSelectIsCurrentPlayer);
   }
-  return { gameBoardArray };
+
+  function oSelectIsCurrentPlayer () {
+    currentPlayer = oMarker.textContent;
+    xMarker.removeEventListener("click", xSelectIsCurrentPlayer);
+  }
+
+  xMarker.addEventListener("click", xSelectIsCurrentPlayer);
+
+  oMarker.addEventListener("click", oSelectIsCurrentPlayer);
+  
+  return { gameBoardArray, showGameBoardArray };
 })();
 
-// Factory function for players in the game.
-const Player = (player, selector) => {
-  const playerName = player;
-  const boardSelector = selector;
-  return { playerName, boardSelector }
-}
+// Selects whichever box is clicked on the game board.
+const boxSelectedOnBoard = (() => {
+  const addSelectedBoxToBoard = () => {
+    const boxSelected = document.querySelectorAll(".box");
+    console.log(boxSelected);
 
-// Individual players created with markers.
-const player = Player("Player", "X");
-const opponent = Player("Opponent", "O");
-
-console.log(player.playerName);
-console.log(player.boardSelector);
-
-// Activates whichever box is selected on the game board.
-(() => {
-  Array.from(document.getElementsByClassName("box"))
-    .forEach(function(element){
-      element.addEventListener("click", function() {
-        const boxID = element;
-        console.log(`${boxID.id} selected.`);
+    boxSelected.forEach((box) => {
+      box.addEventListener("click", () => {
+        console.log(box.id + " works");
       });
     });
+  }
+  
+  return { addSelectedBoxToBoard }
 })();
 
 const gameController = (() => {
-  console.log("Controller for game.");
+  boxSelectedOnBoard.addSelectedBoxToBoard();
 })();
 
 console.log(gameBoard.gameBoardArray);
